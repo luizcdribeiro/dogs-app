c38bf380c9a40bea96f0434d124d4dd9e0870758
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-10-09 00:33:02 -0300

    Componente Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Home from './components/Home.js';
+import Login from './components/Login/Login.js';
 
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
