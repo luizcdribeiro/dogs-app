185245029fda6a1d7be1f1a1bd1bac4e11ba7b21
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-10-19 12:22:11 -0300

    Importação de biblioteca externa

--- src/api.js
+++ src/api.js
@@ -112,7 +112,7 @@ export function PHOTO_DELETE(id) {
 
 export function PASSWORD_LOST(body) {
   return {
-    url: `${API_URL}/api/password/lost`,
+    url: `${API_URL}api/password/lost`,
     options: {
       method: 'POST',
       headers: {
@@ -125,13 +125,25 @@ export function PASSWORD_LOST(body) {
 
 export function PASSWORD_RESET(body) {
   return {
-    url: `${API_URL}/api/password/reset`,
+    url: `${API_URL}api/password/reset`,
     options: {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
+    },
+  };
+}
+
+export function STATS_GET() {
+  return {
+    url: `${API_URL}api/stats`,
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+      },
     },
   };
 }
--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import Head from '../helper/Head.js';
+import useFetch from '../../hooks/useFetch.js';
+import { STATS_GET } from '../../api.js';
+import Loading from '../helper/Loading.js';
+import Error from '../helper/Erro.js';
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs.js'))
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+
+  if (data) {
+    return (
+      <React.Suspense fallback={<Loading />}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  }
+
+  return null;
 };
 
 export default UserStats;
