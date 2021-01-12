211fb6bd9bf37605f4628d036e4a35b2bfce5ba9
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-01-12 10:57:37 -0300

    Updates

---
+++ precache-manifest.eeff548870c4a3a91cb9ae763f753b77.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e511b1bcc6d4f72a1f27732a0a0532",
+    "url": "/dogs-app.github.io/index.html"
+  },
+  {
+    "revision": "b5bf7ff7bc9cd3f322c3",
+    "url": "/dogs-app.github.io/static/css/4.118108ea.chunk.css"
+  },
+  {
+    "revision": "8cd988bda3a2b9bad16c",
+    "url": "/dogs-app.github.io/static/css/main.8b46cd7b.chunk.css"
+  },
+  {
+    "revision": "c65770cc1558d35e794a",
+    "url": "/dogs-app.github.io/static/js/2.ab2347fc.chunk.js"
+  },
+  {
+    "revision": "3aca38d3f3ff93fcd64e",
+    "url": "/dogs-app.github.io/static/js/3.43a55553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dogs-app.github.io/static/js/3.43a55553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bf7ff7bc9cd3f322c3",
+    "url": "/dogs-app.github.io/static/js/4.77e8ed6b.chunk.js"
+  },
+  {
+    "revision": "8cd988bda3a2b9bad16c",
+    "url": "/dogs-app.github.io/static/js/main.48e83979.chunk.js"
+  },
+  {
+    "revision": "7070701035afd273f103",
+    "url": "/dogs-app.github.io/static/js/runtime-main.995175e3.js"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/dogs-app.github.io/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "8cc31290254329d8d3990af08d454cd7",
+    "url": "/dogs-app.github.io/static/media/dogs-footer.8cc31290.svg"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/dogs-app.github.io/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "26c6de43474fd0ef178cb3a9d26c4995",
+    "url": "/dogs-app.github.io/static/media/enviar.26c6de43.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/dogs-app.github.io/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/dogs-app.github.io/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/dogs-app.github.io/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/dogs-app.github.io/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/dogs-app.github.io/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/dogs-app.github.io/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/dogs-app.github.io/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
